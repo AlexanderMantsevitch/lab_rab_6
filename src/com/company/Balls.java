db9904374a68007b0976213175a5b4c770747920
Alex <rct.mancevic@bsu.by>
2022-03-30 18:26:23 +0300

    Конструктор Balls

--- src/com/company/Balls.java
+++ src/com/company/Balls.java
@@ -2,12 +2,18 @@ package com.company;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
 
 public class Balls implements Runnable {
 
     private Field field;
     private Color color;
-    private double Radius;
+    private int Radius;
+    private int MaxRadius = 35;
+    private int MinRadius = 2;
+
+    private double MaxSpeed = 14;
+
 
     private double x;
     private double y;
@@ -15,6 +21,24 @@ public class Balls implements Runnable {
     private double speed;
     private double speedX;
     private double speedY;
+
+    public Balls ()
+    {
+        Radius = (int)(Math.random()* (MaxRadius - MinRadius )) + MinRadius;
+        speed = Math.round(7 * MaxSpeed/Radius);
+        if ( speed > MaxSpeed) {speed = MaxSpeed; }
+        double ange = Math.random() * 2 * Math.PI;
+        speedX = speed * Math.cos(ange);
+        speedY = speed * Math.sin(ange);
+        color = new Color ( (float) Math.random(), (float)Math.random(),(float)Math.random());
+        x =  (Math.random() * (field.getWidth() - 2 * Radius )) + Radius;
+        x =  (Math.random() * (field.getHeight() - 2 * Radius )) + Radius;
+
+        Thread NewThread = new Thread(this);
+        NewThread.start();
+    }
+
+
    public void run()
     {
         try {
