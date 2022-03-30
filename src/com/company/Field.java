7733c74dd5eb3b789b86122078dd32a6151c6396
Alex <rct.mancevic@bsu.by>
2022-03-30 17:46:09 +0300

    paintComponent

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -4,16 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Field extends JPanel {
 
     private boolean paused;
+
+    private ArrayList<Balls> balls = new ArrayList<Balls>(3);
+    public void paintComponent(Graphics g) {
+// Вызвать версию метода, унаследованную от предка
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+// Последовательно запросить прорисовку от всех мячей из списка
+        for (Balls ball: balls) {
+
+        }
+    }
+
+
     private Timer repaintTime = new Timer(10, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
            repaint();
         }
     });
+
+
+
+
+
     public Field ()
     {
         setBackground(Color.white);
